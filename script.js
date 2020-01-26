f301ef61d62b1d24e6260b68adfe1b241c61fd31
Timework <timework.bibo@gmail.com>
2020-01-25 23:21:46 -0600

    works but ugly

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var container = document.querySelector("#container");
 var numberOfDivs = 16
-var dimensions = 500/numberOfDivs + "px"
 
 container.classList.add("container");
 
 function grid(numberOfDivs){
     for (i = 0; i < Math.pow(numberOfDivs, 2); i++){
+        var dimensions = 500 / numberOfDivs + "px"
         let square = document.createElement("div");
         square.classList.add("square");
         square.style.height = dimensions
@@ -19,6 +19,47 @@ function grid(numberOfDivs){
     function shading(e) {
         e.target.style.background = "black";
     }
+    document.getElementById("clear").addEventListener("click", clearGrid);
+    function clearGrid() {
+        document.querySelectorAll("div.square").forEach((reset) => {
+            reset.style.background = "white";
+        })
+    }
+    document.getElementById("random").addEventListener("click", randomGrid);
+    function randomGrid() {
+        document.querySelectorAll("div.square").forEach((color) => {
+            clearGrid()
+            let r = Math.random() * 255
+            let g = Math.random() * 255
+            let b = Math.random() * 255
+            color.addEventListener("mouseover", colorRandom);
+            function colorRandom(e) {
+                e.target.style.background = `rgba(${r},${g},${b})`
+            }})}
+    document.getElementById("grayscale").addEventListener("click", shadingGrid);
+    function shadingGrid() {
+        clearGrid()
+        document.querySelectorAll("div.square").forEach((gray) => {
+            var grayItUp = 0
+            gray.addEventListener("mouseover", grayColor);
+            function grayColor(e) {
+                grayItUp += .1
+                e.target.style.background = `rgba(0,0,0,${grayItUp}`
+                
+            }
+
+        })
+    }
+
 
 }
 grid(numberOfDivs);
+
+document.getElementById("newGrid").addEventListener("click", sizeGrid);
+function sizeGrid() {
+    howManyDivs = prompt("Dimensions of new grid.");
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    grid(howManyDivs);
+}
