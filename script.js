4d4c58a74c328f25fcd219814414503fc149484f
Timework <timework.bibo@gmail.com>
2020-01-25 21:41:30 -0600

    little more organized

---
+++ script.js
@@ -0,0 +1,17 @@
+var container = document.querySelector("#container");
+var numberOfDivs = 16
+var dimensions = 500/numberOfDivs + "px"
+
+container.classList.add("container");
+
+function grid(numberOfDivs){
+    for (i = 0; i < Math.pow(numberOfDivs, 2); i++){
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.height = dimensions
+        square.width = dimensions
+        square.textContent = "test"
+        container.appendChild(square);
+    }
+}
+console.log(grid(numberOfDivs))
